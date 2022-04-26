2b436bc0532036b3574c780307e326e9c9e4707d
Nailton <51789882+nailtonvital@users.noreply.github.com>
2022-04-25 23:36:55 -0300

    main

---
+++ app/Controllers/Http/UsersController.ts
@@ -0,0 +1,22 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User';
+
+
+export default class UsersController {
+    public async create({ request }: HttpContextContract) {
+        const { username, name } = request.only(["name", "username"]);
+
+        const user = await User.create({
+            username,
+            name
+        })
+
+        return user;
+    }
+
+    public async index(){
+        const all = User.all();
+
+        return all;
+    }
+}
